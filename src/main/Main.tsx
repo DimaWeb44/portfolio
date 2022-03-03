15b21400a7898f466c728213ca4ecbf4e09b41d9
DimaWeb44 <dima.web44@gmail.com>
2022-03-04 00:08:02 +0300

    init

---
+++ src/main/Main.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import style from './Main.module.css';
+import styleContainer from '../common/styles/Container.module.css'
+
+function Main() {
+    return (
+        <div className={style.mainBlock}>
+            <div className={styleContainer.container}>
+                <div className={style.text}>
+                    <span>Hi There</span>
+                    <h1>I am Dmitry Petukhov</h1>
+                    <p>Frontend Developer</p>
+                </div>
+                <div className={style.photo}></div>
+            </div>
+        </div>
+    );
+}
+
+export default Main;
+
+
---
+++ src/skills/Skills.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import style from './Skills.module.css';
+import styleContainer from '../common/styles/Container.module.css'
+import Skill from "./skill/Skill";
+
+function Skills() {
+    return (
+        <div className={style.skillsBlock}>
+          <div className={`${styleContainer.container} ${style.skillsContainer}`}>
+              <h2 className={style.title}>My Skills</h2>
+              <div className={style.skills}>
+                  <Skill title={'React'} description={'Lorem Ipsum is simply dummy text of the printing ' +
+                  'and typesetting industrys standard'}/>
+                  <Skill title={'TypeScript'} description={'Lorem Ipsum is simply dummy text of the printing ' +
+                  'and typesetting industry.'}/>
+                  <Skill title={'JavaScript'} description={'Lorem Ipsum  printing ' +
+                  'and typesetting industry. Lorem Ipsum has been the industrys standard'}/>
+              </div>
+          </div>
+        </div>
+    );
+}
+
+export default Skills;
+
+
