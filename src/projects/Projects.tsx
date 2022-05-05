341a81bd02bdd8f9b81bbad71f00141d6657ec32
DimaWeb44 <dima.web44@gmail.com>
2022-05-05 22:55:56 +0300

    upgrade3

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
-import style from './Projects.module.css';
+import style from './Projects.module.scss';
 import styleContainer from '../common/styles/Container.module.css'
-import Project from "./project/Project";
+import Project from './project/Project';
 import Title from '../common/components/title/Title';
+import img from '../assets/image/look.com.ua-200911.jpg'
+import img2 from '../assets/image/look.com.ua-191288.jpg'
+
 
 
 function Projects() {
+
+    const socialNetworkStyle = {
+        backgroundImage: `url(${img})`,
+    };
+
+    const todolistStyle = {
+        backgroundImage: `url(${img2})`,
+    };
+
     return (
         <div className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
                 <Title title={'My Projects'}/>
                 <div className={style.projects}>
-                    <Project title={'Social network'} description={'Lorem Ipsum is simply dummy text of the printing ' +
+                    <Project style={socialNetworkStyle} title={'Social network'} description={'Lorem Ipsum is simply dummy text of the printing ' +
                     'and typesetting industrys standard'}/>
-                    <Project title={'Todolist'} description={'Lorem Ipsum is simply dummy text of the printing ' +
+                    <Project style={todolistStyle} title={'Todolist'} description={'Lorem Ipsum is simply dummy text of the printing ' +
                     'and typesetting industry.'}/>
                 </div>
             </div>
--- src/projects/project/Project.tsx
+++ src/projects/project/Project.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import style from './Project.module.css';
+import style from './Project.module.scss';
 
 function Project(props: any) {
     return (
         <div className={style.project}>
-            <div className={style.icon}>
+            <div style={props.style} className={style.icon}>
                 <button className={style.buttonIcon}>browse</button>
             </div>
-            <h3 className={style.h3}>{props.title}</h3>
+            <div className={style.projectInfo}>
+            <h3 className={style.projectTitle}>{props.title}</h3>
             <span className={style.description}>{props.description}</span>
+            </div>
         </div>
     );
 }
