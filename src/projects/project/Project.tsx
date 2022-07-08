11c9021cd9a46557794137eed1687d8ba5986470
DimaWeb44 <dima.web44@gmail.com>
2022-07-09 00:51:01 +0300

    upgrade7

--- src/projects/project/Project.tsx
+++ src/projects/project/Project.tsx
@@ -5,7 +5,11 @@ import { AnimationWrapper } from 'react-hover-animation'
 function Project(props: any) {
     return (
         <div className={style.project}>
-            <a href={props.link} style={props.style} className={style.icon}></a>
+            <div className={style.image}>
+                <a href={props.link} style={props.style} className={style.img}></a>
+                <a href={props.link} className={style.imageTitle}>DEMO</a>
+            </div>
+
             <div className={style.projectInfo}>
             <h3 className={style.projectTitle}>{props.title}</h3>
             <span className={style.description}>{props.description}</span>
